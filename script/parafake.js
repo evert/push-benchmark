5ab39b6a91e15ddd884a5a268b1ea562347c6938
Evert Pot <me@evertpot.com>
2019-12-22 00:02:54 -0500

    Block renderer

--- script/parafake.js
+++ script/parafake.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', main);
 const testData = {
 
   'h1-nocache': {
-    httpVersion: '1.1'
+    httpVersion: '1.1',
   },
 
 }
@@ -24,12 +24,17 @@ const gridSize = 200;
 
 function loadTestSample(elem, id) {
 
-  renderTemplate(elem);
-  renderGrid(elem.getElementByClassName('blocks'));
+  const test = testData[id];
+  renderTemplate(elem, test);
+  const grid = renderGrid(elem.getElementsByClassName('blocks'));
+
+  elem.getElementsByTagName('button', () => {
+    startTest(test, grid);
+  });
 
 }
 
-function renderTemplate(elem)  {
+function renderTemplate(elem, test)  {
 
   elem.innerHTML = `<h3>${gridSize} requests via HTTP/${test.httpVersion}</h3>
 <div class="blocks">
@@ -39,7 +44,6 @@ function renderTemplate(elem)  {
 `;
 
 }
-
 
 function renderGrid(elem) {
   const grid = [];
@@ -52,4 +56,92 @@ function renderGrid(elem) {
   }
 
   return grid;
+}
+
+function delay(ms) {
+  return new Promise( res => {
+    setTimeout(res, ms);
+  });
+}
+
+async function startTest(test, grid) {
+
+  for(const cell of grid) {
+    cell.className = '';
+  }
+
+  const promises = [];
+  const throttler = new RequestThrottler(6);
+
+  for(const cell of grid) {
+    // Adding a tiny delay since there is a small
+    // amount of overhead in kicking off a request
+    await delay(10);
+
+    promises.push(async () => {
+      await throttler.go()
+      cell.className = 'received';
+    });
+  }
+
+  await Promise.all(promises);
+
+}
+
+const minLatency = 100;
+const maxLatency = 150;
+
+class RequestThrottler {
+
+  constructor(maxConcurrency) {
+
+    this.maxConcurrency = maxConcurrency;
+    this.queuedRequests = [];
+    this.inFlightCount = 0;
+
+  }
+
+  async go() {
+
+    let resolver;
+
+    // This is the promise we're eventually resolving
+    const resultPromise = new Promise(res, rej => {
+      resolver = res;
+    });
+
+    if (this.inFlightCount < this.maxConcurrency) {
+
+      await this.request();
+      resolver();
+      this.checkQueue();
+
+    } else {
+      this.queuedRequests.push(resolver);
+    }
+
+    return resultPromise;
+
+  }
+
+  async request() {
+
+    this.inFlightCount++;
+    await delay(Math.random() * (maxLatency - minLatency) + minLatency);
+    this.inFlightCount--;
+
+  }
+
+  checkQueue() {
+
+    if (this.inFlightCount < this.maxConcurrency && this.queuedRequests.length) {
+
+      const resolver = this.queuedRequests.shift();
+      await this.request();
+      resolver();
+
+    }
+
+  }
+
 }
